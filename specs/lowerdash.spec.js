e0c7d9b53f37d0dbdcf049d73d7e76f2b7fe4207
Sai Gudigundla <saip106@msn.com>
2014-07-27 21:51:08 -0400

    added a negative test case for difference function

--- specs/lowerdash.spec.js
+++ specs/lowerdash.spec.js
@@ -4,7 +4,7 @@ describe('underscore', function () {
         it('should be defined', function () {
             expect(_).toBeDefined();
         });
-    })
+    });
 
     describe('when compacting an array', function () {
         it('should remove falsey values from the array', function () {
@@ -15,10 +15,16 @@ describe('underscore', function () {
 
     describe('when doing a difference on an array', function () {
         it('should remove the values specified', function () {
-            var foo = {};
-            var array = [1,2,3, foo];
-            expect(_.difference(array, foo)).toEqual([1,2,3]);
-        })
+            var foo = { bar: 123 };
+            var array = [1, 2, 3, foo];
+            expect(_.difference(array, foo)).toEqual([1, 2, 3]);
+        });
+
+        it('should not remove values that are not equal by ===', function () {
+            var foo = { bar: 123 };
+            var array = [1, 2, 3, { bar: 123}];
+            expect(_.difference(array, foo)).toEqual([1, 2, 3, { bar: 123}]);
+        });
     });
 
 })
