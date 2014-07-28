6cc37d4ff09a2033d0946e70224afc0b853dc138
Sai Gudigundla <saip106@msn.com>
2014-07-27 20:31:42 -0400

    implemented compact function

--- lowerdash.js
+++ lowerdash.js
@@ -1,5 +1,47 @@
 (function () {
-    var compact = function (array) {
+
+    // Establish the root object, `window` in the browser, or `exports` on the server.
+    var root = this;
+
+
+    var _ = function () {
 
     }
-}).call(this);
+
+    // Export the Underscore object for **Node.js**, with
+    // backwards-compatibility for the old `require()` API. If we're in
+    // the browser, add `_` as a global object.
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = _;
+        }
+        exports._ = _;
+    } else {
+        root._ = _;
+    }
+
+    _.compact = function (array) {
+        var result = [];
+        for(var i = 0; i < array.length; i++) {
+            if (array[i]) {
+                result[result.length] = array[i];
+            }
+        }
+        return result;
+    }
+
+
+    // AMD registration happens at the end for compatibility with AMD loaders
+    // that may not enforce next-turn semantics on modules. Even though general
+    // practice for AMD registration is to be anonymous, underscore registers
+    // as a named module because, like jQuery, it is a base library that is
+    // popular enough to be bundled in a third party lib, but not be part of
+    // an AMD load request. Those cases could generate an error when an
+    // anonymous define() is called outside of a loader request.
+    if (typeof define === 'function' && define.amd) {
+        define('underscore', [], function() {
+            return _;
+        });
+    }
+
+}.call(this));
--- specs/lowerdash.spec.js
+++ specs/lowerdash.spec.js
@@ -1,6 +1,9 @@
-describe('dummy spec', function () {
-    it('should run the test', function () {
+describe('underscore', function () {
+
+    it('should should be defined', function () {
         var array = [0,1,2];
-        
+        expect(_.compact(array)).toEqual([1,2]);
     });
+
+
 })
