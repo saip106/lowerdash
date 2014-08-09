97511c88e439f827c514c1a7f0da513b6e6b9064
Sai Gudigundla <saip106@msn.com>
2014-08-09 11:16:36 -0400

    attempt to get linting work with jake

--- jakefile.js
+++ jakefile.js
@@ -1,6 +1,8 @@
 'use strict';
 
-task('default', ['test'], function () {
+var jshint = require('jshint').JSHINT;
+
+task('default', ['lint', 'test'], function () {
 
 });
 
@@ -8,4 +10,18 @@ task('test', [], function (params) {
     jake.exec(['karma start'], { printStdout: true }, function () {
         complete();
     });
+});
+
+task('lint', function () {
+    var fileNameList = new jake.FileList();
+    fileNameList.include('*.js');
+    fileNameList.exclude('karma.conf.js');
+
+    fileNameList.forEach(function (fileName) {
+            
+    });
+    for(var i = 0; i < fileNameList.length; i++) {
+        var sourceCode = require("fs").readFileSync(fileNameList);
+        var  jshint(sourceCode, {}, {});
+    }
 });
