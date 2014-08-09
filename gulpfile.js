b287a5f413db4ad76b39f353f232b10af677fe55
Sai Gudigundla <saip106@msn.com>
2014-08-09 14:33:01 -0400

    got karma running with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,5 +6,13 @@ var karma = require('gulp-karma');
 gulp.task('default', []);
 
 gulp.task('test', function () {
-
+    gulp.src(['*.js', '**/*.spec.js'])
+        .pipe(karma({
+            configFile: 'karma.conf.js',
+            action: 'watch'
+        }))
+        .on('error', function(err) {
+            // Make sure failed tests cause gulp to exit non-zero
+            throw err;
+        });
 });
